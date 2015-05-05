b2c4c3e460dd831d2c436501f3c655a6e9a92daf
Sara Metz <sara.metz@koordinates.com>
2015-05-05 16:07:43 +1200

    Added exception handling for many to many fields that have not yet been saved

--- rest_framework/fields.py
+++ rest_framework/fields.py
@@ -73,7 +73,7 @@ def set_component(obj, attr_name, value):
     else:
         try:
             attr = getattr(obj, attr_name, None)
-        except ObjectDoesNotExist:
+        except (ObjectDoesNotExist, ValueError):
             # happens for non-null FK fields that haven't yet been set.
             pass
         else:
